d6f6a682ad467983b5da8a0ed8387a87a4258a76
Minhazur Rahman <iamrafi404@gmail.com>
2021-04-30 10:32:40 -0400

    gajaa

--- Projectsln/Project/Customer_add.Designer.cs
+++ Projectsln/Project/Customer_add.Designer.cs
@@ -183,9 +183,9 @@
             // 
             this.dt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dt1.Location = new System.Drawing.Point(158, 288);
-            this.dt1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dt1.Margin = new System.Windows.Forms.Padding(2);
             this.dt1.Name = "dt1";
-            this.dt1.Size = new System.Drawing.Size(204, 23);
+            this.dt1.Size = new System.Drawing.Size(230, 23);
             this.dt1.TabIndex = 15;
             // 
             // CustomerInfo
